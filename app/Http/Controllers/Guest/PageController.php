6d5aa8ff8bb1624380ee467ffe1cd74a7e3d3788
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-06-29 10:46:32 +0200

    aggiunte immagini

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index()
     {   
-        $link = config('Models.movies');
         $movies = Movie::all();
         return view('movies' , compact('movies'));
     }
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,11 +2,12 @@
 @section('content')
 @foreach ($movies as $movie)
     <div class="card">
-        <h3>Titolo:{{ $movie->title }}</h3>
-        <p>Descrizione:{{ $movie->description }}</p>
-        <p>Titolo originale:{{$movie->original_title}}</p>
-        <p>Nazione:{{$movie->nationality}}</p>
-        <p>data:{{$movie->date}}</p>
+        <h3>Titolo: {{ $movie->title }}</h3>
+        <img style="width: 200px" src="{{$movie->img}}" alt="{{$movie->title}} ">
+        <p>Descrizione: {{ $movie->description }}</p>
+        <p>Titolo originale: {{$movie->original_title}}</p>
+        <p>Nazione: {{$movie->nationality}}</p>
+        <p>data: {{$movie->date}}</p>
         <p>voto: {{$movie->vote}}</p>
     </div>
 @endforeach
